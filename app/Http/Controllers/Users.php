084e53f82593ad721e824ec889ee5a804055b98b
Gowtham708 <rgowtham708@gmail.com>
2021-05-17 10:35:39 +0530

    create the models

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\sample;
 
 class Users extends Controller
 {
     //
     function index()
     {
-        return view('sample',['name'=>'Gowtham']);
+        return sample::all();
+        //return view('sample',['name'=>'Gowtham']);
         //return ['name'=>'Gowtham'];
     }
 }
